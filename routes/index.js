80fc4872e069568179a415bf351c1bf186d4312e
Arturo Perez <VrturoPerez@gmail.com>
2015-09-17 19:47:24 -0700

    add form to add users

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,19 @@ router.get('/helloworld', function(req, res) {
     res.render('helloworld', { title: 'Hello, World!' });
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,users){
+    res.render('userlist', {
+      "users" : users
+    });
+  });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
 module.exports = router;
